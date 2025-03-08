4788bbb5638f1bb7dd28ae42ccbd339e870d7509
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-08 08:27:15 -0300

    Creacion de paginas para los diversos enlaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import {Home} from './Home'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Services from './pages/Services.jsx'
+import Contact from './pages/Contact.jsx'
 import './App.css'
+import Menu from './pages/Menu.jsx'
 
 function App() {
   
   return (
     <>
       <HashRouter>
+        <Menu />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/services" element={<h1>Services</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/" element={<h1><Home/></h1>} />
+          <Route path="/about" element={<h1><About/></h1>} />
+          <Route path="/services" element={<h1><Services/></h1>} />
+          <Route path="/contact" element={<h1><Contact/></h1>} />
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </HashRouter>
