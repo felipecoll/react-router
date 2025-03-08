6fecdd7974bf80c371bc4c552362686d9c921e56
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-08 09:09:18 -0300

    Navlink, isActive y map con routes - simplificacion de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
       <HashRouter>
         <Menu />
         <Routes>
-          <Route path="/" element={<h1><Home/></h1>} />
-          <Route path="/about" element={<h1><About/></h1>} />
-          <Route path="/services" element={<h1><Services/></h1>} />
-          <Route path="/contact" element={<h1><Contact/></h1>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/services" element={<Services/>} />
+          <Route path="/contact" element={<Contact/>} />
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </HashRouter>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const routes = [
+    { path: '/', name: 'Home' },
+    { path: '/about', name: 'About' },
+    { path: '/services', name: 'Services' },
+    { path: '/contact', name: 'Contact' }
+]
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <nav>
+      <ul>
+        {routes.map((route, index) => (
+          <li key={index}>
+            <NavLink 
+              style={({ isActive }) => ({
+                color: isActive ? 'blue' : 'white'
+              })} 
+              to={route.path} 
+              end
+            >
+              {route.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
